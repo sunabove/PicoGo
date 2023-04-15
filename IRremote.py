b8e724527cb9b8031cef2bb99e126786ae4ec47f
sunabove <sunabove@nate.com>
2023-04-15 19:50:22 +0900

    **

--- IRremote.py
+++ IRremote.py
@@ -1,10 +1,6 @@
 from machine import Pin
 from Motor import PicoGo
-import utime
-
-IR = Pin(5, Pin.IN)
-M = PicoGo()
-speed = 50
+from time import sleep_us
 
 def getkey():
     global IR
@@ -12,13 +8,13 @@ def getkey():
         count = 0
         while ((IR.value() == 0) and (count < 100)): #9ms
             count += 1
-            utime.sleep_us(100)
+            sleep_us(100)
         if(count < 10):
             return None
         count = 0
         while ((IR.value() == 1) and (count < 50)): #4.5ms
             count += 1
-            utime.sleep_us(100)
+            sleep_us(100)
             
         idx = 0
         cnt = 0
@@ -27,12 +23,12 @@ def getkey():
             count = 0
             while ((IR.value() == 0) and (count < 10)):    #0.56ms
                 count += 1
-                utime.sleep_us(100)
+                sleep_us(100)
 
             count = 0
             while ((IR.value() == 1) and (count < 20)):   #0: 0.56mx
                 count += 1                                #1: 1.69ms
-                utime.sleep_us(100)
+                sleep_us(100)
 
             if count > 7:
                 data[idx] |= 1<<cnt
@@ -46,40 +42,43 @@ def getkey():
             return data[2]
         else:
             return("repeat")
+
+if __name__=='__main__':
     
-n = 0   
-while True:
-    key = getkey()
-    if(key != None):
-        n = 0
-        if key == 0x18:
-            M.forward(speed)
-            print("forward")
-        if key == 0x08:
-            M.left(20)
-            print("left")
-        if key == 0x1c:
-            M.stop()
-            print("stop")
-        if key == 0x5a:
-            M.right(20)
-            print("right")
-        if key == 0x52:
-            M.backward(speed)
-            print("backward")
-        if key == 0x09:
-            speed = 50
-            print(speed)
-        if key == 0x15:
-            if(speed + 10 < 101):
-                speed += 10
-            print(speed)
-        if key == 0x07:
-            if(speed - 10 > -1):
-                speed -= 10
-            print(speed)
-    else:
-        n += 1
-        if n > 800:
-            n = 0
-            M.stop()
+    IR = Pin(5, Pin.IN)
+    M = PicoGo()
+    speed = 50
+    
+    while True:
+        key = getkey()
+        if(key != None):
+            if key == 0x18:
+                M.forward(speed)
+                print("forward")
+            elif key == 0x08:
+                M.left(20)
+                print("left")
+            elif key == 0x1c:
+                M.stop()
+                print("stop")
+            elif key == 0x5a:
+                M.right(20)
+                print("right")
+            elif key == 0x52:
+                M.backward(speed)
+                print("backward")
+            elif key == 0x09:
+                speed = 50
+                print(speed)
+            elif key == 0x15:
+                if(speed + 10 < 101):
+                    speed += 10
+                print(speed)
+            elif key == 0x07:
+                if(speed - 10 > -1):
+                    speed -= 10
+                print(speed)
+            pass
+        pass
+    pass
+pass
