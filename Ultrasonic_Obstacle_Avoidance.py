eecc3fb6acf1145be817432b739ee85b0c08bec9
sunabove <sunabove@nate.com>
2023-04-17 19:06:45 +0900

    .

--- Ultrasonic_Obstacle_Avoidance.py
+++ Ultrasonic_Obstacle_Avoidance.py
@@ -2,31 +2,33 @@ import utime
 from machine import Pin
 from Motor import PicoGo
 
-M = PicoGo()
-Echo = Pin(15, Pin.IN)
-Trig = Pin(14, Pin.OUT)
-Trig.value(0)
-Echo.value(0)
+robot = PicoGo()
+echo = Pin(15, Pin.IN)
+trig = Pin(14, Pin.OUT)
 
-def dist():
-    Trig.value(1)
+trig.value(0)
+echo.value(0)
+
+def get_distance():
+    trig.value(1)
     utime.sleep_us(10)
-    Trig.value(0)
-    while(Echo.value() == 0):
+    trig.value(0)
+    while echo.value() == 0 :
         pass
     ts=utime.ticks_us()
-    while(Echo.value() == 1):
+    while echo.value() == 1:
         pass
     te=utime.ticks_us()
     distance=((te-ts)*0.034)/2
+    
     return distance
 
 while True:
-    D = dist()
-    if(D <= 20):
-        M.right(20)
-        #Ab.left()
+    dist = get_distance()
+    if dist <= 20 :
+        robot.right(20)
     else:
-        M.forward(20)
+        robot.forward(20)
         
     utime.sleep_ms(20)
+pass
