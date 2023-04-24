2eb401565b569fb47c7b36d833a74d787b40982c
sunabove <sunabove@nate.com>
2023-04-24 18:23:15 +0900

    Update BatteryVoltage.py

--- BatteryVoltage.py
+++ BatteryVoltage.py
@@ -17,27 +17,28 @@ if __name__ == '__main__' :
     lcd.line(17,132,237,132,0xBB56)
     lcd.line(237,32,237,132,0xBB56)
 
-    lcd.text("Raspberry Pi Pico",90,7,0xFF00)
-    lcd.text("PicoGo",10,7,0x001F)
-    lcd.text("Waveshare.com",70,120,0x07E0)
+    lcd.text("Raspberry Pi Pico", 90, 7, 0xFF00)
+    lcd.text("PicoGo", 10, 7, 0x001F)
+    lcd.text("Waveshare.com", 70, 120, 0x07E0)
     lcd.show()
 
     count = 1
     while True :
-        reading = temp.read_u16() * 3.3 / (65535)
-        temperature = 27 - (reading - 0.706)/0.001721
-        v = bat.read_u16()*3.3/65535 * 2
-        p = (v - 3) * 100 / 1.2
+        reading = temp.read_u16()*3.3/65535
         
-        if(p < 0): p=0
-        if(p > 100): p=100
+        temperature = 27 - (reading - 0.706)/0.001721
+        voltage = bat.read_u16()*3.3/65535*2
+        percent = (voltage - 3)*100/1.2
+        
+        if percent < 0: percent = 0
+        if percent > 100 : percent = 100
 
         y = 35
         lcd.fill_rect(30, y, 120, 40,0xF232)
         lcd.text( f"count : {count}", 30, y, 0xFFFF ); y += 15
         lcd.text( f"temperature : {temperature:5.2f} C", 30, y, 0xFFFF ); y += 15
-        lcd.text( f"Voltage     : {v:5.2f} V", 30, y, 0xFFFF ); y += 15
-        lcd.text( f"percent     : {p:3.1f} %", 30, y, 0xFFFF ); y += 15
+        lcd.text( f"Voltage     : {voltage:5.2f} V", 30, y, 0xFFFF ); y += 15
+        lcd.text( f"percent     : {percent:3.1f} %", 30, y, 0xFFFF ); y += 15
 
         lcd.show()
         sleep( 1 )
