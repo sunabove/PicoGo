f90ffbb7b2ecfc87ba50de38ba6091f009190447
sunabove <sunabove@nate.com>
2023-06-11 16:59:09 +0900

    **

--- bluetooth.py
+++ bluetooth.py
@@ -24,7 +24,7 @@ lcd.line(237,32,237,132,0xBB56)
 
 lcd.text("Raspberry Pi Pico",90,7,0xFF00)
 lcd.text("PicoGo",10,7,0x001F)
-lcd.text("Waveshare.com",70,120,0x07E0)
+lcd.text("SkySLM Coorp.",70,120,0x07E0)
 lcd.show()
 
 motor = Motor()
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 if __name__== '__main__' :
+    
     print( 'Hello PicoGo!' )
     print( 'Running Bluetooth program ... ' )
     
-    import Bluetooth
+    import BlueTooth
     
 pass
