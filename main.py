88c7db3400d25a5e164f24473181f148172cadbe
sunabove <sunabove@nate.com>
2023-10-27 19:59:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 if __name__== '__main__' :
     
+    import sys
+    
+    print( f'System Version = {sys.version}' )
     print( 'Hello PicoGo!' )
     print( 'Running Bluetooth program ... ' )
-    
+        
     from picogo import BlueTooth
     
     BlueTooth.main()
